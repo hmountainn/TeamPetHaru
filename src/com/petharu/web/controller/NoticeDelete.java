7a1215c8c2e2b3f26887daa6703bbaa1ef6c951a
hyo2n <hyo_2n@naver.com>
2021-05-20 13:09:59 +0900

    controller 추가

---
+++ src/com/petharu/web/controller/NoticeDelete.java
@@ -0,0 +1,43 @@
+package com.petharu.web.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.petharu.web.service.NoticeService;
+
+
+@WebServlet("/community/notice/delete")
+public class NoticeDelete extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		int id = Integer.parseInt(req.getParameter("id"));
+
+		NoticeService noticeService = new NoticeService();
+		
+		try {
+			noticeService.delete(id);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		resp.sendRedirect("list.html");
+	}
+
+}
