e0105171bcf04eb93f2ab52402712e805d5f2827
이민주 <aamj0226@gmail.com>
2021-05-20 15:18:13 +0900

    Add knowhow 페이지

---
+++ WebContent/knowhow/reg.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function() {
+	
+	let petInput = document.querySelector(".pet-selecting-btn>input");
+	let dogBtn = document.querySelectorAll(".pet-selecting-btn>span")[0];
+	let catBtn = document.querySelectorAll(".pet-selecting-btn>span")[1];
+
+	// select라는 클래스를 가지는 태그의 name 속성과 p값 추가하기
+	dogBtn.onclick = function(e) {
+		catBtn.classList.remove("select");		
+		dogBtn.classList.add("select");	
+		petInput.removeAttribute('value', 'cat');
+		petInput.setAttribute('value', 'dog');
+	}
+	
+	catBtn.onclick = function(e) {
+		dogBtn.classList.remove("select");		
+		catBtn.classList.add("select");	
+		petInput.removeAttribute('value', 'dog');
+		petInput.setAttribute('value', 'cat');
+	}
+	
+});
+
+
