48dd7372b37aa0b055390650516aa9e5643d4e92
hmountainn <ghkdwltks123@naver.com>
2021-05-20 16:47:50 +0900

    Member 수정

--- src/com/petharu/web/entity/Member.java
+++ src/com/petharu/web/entity/Member.java
@@ -2,7 +2,7 @@ package com.petharu.web.entity;
 
 public class Member {
 	private int id;
-	private String user_id;
+	private String userId;
 	private String email;
 	private String password;
 	private String image;
@@ -16,7 +16,7 @@ public class Member {
 	public Member(int id, String user_id, String email, String password, String image) {
 		super();
 		this.id = id;
-		this.user_id = user_id;
+		this.userId = user_id;
 		this.email = email;
 		this.password = password;
 		this.image = image;
@@ -33,11 +33,11 @@ public class Member {
 	}
 
 	public String getUser_id() {
-		return user_id;
+		return userId;
 	}
 
 	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+		this.userId = user_id;
 	}
 
 	public String getEmail() {
@@ -66,7 +66,7 @@ public class Member {
 
 	@Override
 	public String toString() {
-		return "Member [id=" + id + ", user_id=" + user_id + ", email=" + email + ", password=" + password + ", image="
+		return "Member [id=" + id + ", userId=" + userId + ", email=" + email + ", password=" + password + ", image="
 				+ image + "]";
 	}
 	
