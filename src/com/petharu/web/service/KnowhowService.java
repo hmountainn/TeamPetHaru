3d804b942552ea1a1b4f15660bf514bfade725e7
이민주 <aamj0226@gmail.com>
2021-05-20 15:07:00 +0900

    Add KnowhowService

---
+++ src/com/petharu/web/service/KnowhowService.java
@@ -0,0 +1,20 @@
+package com.petharu.web.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.petharu.web.entity.Knowhow;
+
+public interface KnowhowService {
+
+	List<Knowhow> getList();
+	List<Knowhow> getList(int page);
+
+}
