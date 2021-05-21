175727a260f9f5d7edc91a5fc4b20d4fbd083bbc
hyo2n <hyo_2n@naver.com>
2021-05-22 08:45:17 +0900

    DiaryComment, Notice detail 수정

---
+++ src/com/petharu/web/service/DiaryCommentService.java
@@ -0,0 +1,16 @@
+package com.petharu.web.service;
+
+import java.util.List;
+
+import com.petharu.web.entity.DiaryComment;
+
+public interface DiaryCommentService {
+	
+	List<DiaryComment> getList(int id);
+	
+	int delete(int id);
+	int insert(DiaryComment diaryComment);
+	int update(DiaryComment diaryComment);
+	int reply(DiaryComment diaryComment);
+
+}
