08ee6b4df458cdd24cc1fe7f4b415fdd7e34818c
hyo2n <hyo_2n@naver.com>
2021-05-21 09:24:48 +0900

    NoticeList 수정

--- src/com/petharu/web/api/NoticeList.java
+++ src/com/petharu/web/api/NoticeList.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.petharu.web.entity.Notice;
 import com.petharu.web.service.JDBCNoticeService;
+import com.petharu.web.service.NoticeService;
 
 
 @WebServlet("/api/notice/list")
@@ -47,7 +48,7 @@ public class NoticeList extends HttpServlet {
 				query = q;
 			}
 			
-			JDBCNoticeService noticeService = new JDBCNoticeService();
+			NoticeService noticeService = new JDBCNoticeService();
 			List<Notice> list = noticeService.getList(page, field, query);
 
 			Gson gson = new Gson();
