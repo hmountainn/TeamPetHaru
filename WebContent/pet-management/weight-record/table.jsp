a8dd343c5301323bdfdda731be0c377a3dd33d61
hmountainn <ghkdwltks123@naver.com>
2021-05-21 21:41:25 +0900

    WeightService getList()수정

--- WebContent/pet-management/weight-record/table.jsp
+++ WebContent/pet-management/weight-record/table.jsp
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="table-style.css" type="text/css" rel="stylesheet">
-    <script src="table.js"></script>
+<!--     <script src="table.js"></script> -->
 </head>
 <body>
     <div id="root">
--- src/com/petharu/web/service/JDBCWeightService.java
+++ src/com/petharu/web/service/JDBCWeightService.java
@@ -2,23 +2,25 @@ package com.petharu.web.service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 
 import com.petharu.web.entity.Weight;
 
-public class JDBCWeightService implements WeightService{
+public class JDBCWeightService implements WeightService {
 	public List<Weight> getList(){
 		List<Weight> list = new ArrayList<>();
 		
 		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
 		String sql = "SELECT * FROM WEIGHT";
+		
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver"); //드라이버 생성
@@ -29,15 +31,13 @@ public class JDBCWeightService implements WeightService{
 			while(rs.next()) {
 				int id = rs.getInt("ID");
 				int petId = rs.getInt("PET_ID");
-				Date measureDate = rs.getDate("MEASURE_DATE");
-				Time measureTime = rs.getTime("MEASURE_TIME");
+				String measureDatetime = rs.getString("MEASURE_DATETIME");
 				float kg = rs.getFloat("KG");
 				
 				Weight weight = new Weight();
 				weight.setId(id);
 				weight.setPetId(petId);
-				weight.setMeasureDate(measureDate);
-				weight.setMeasureTime(measureTime);
+				weight.setMeasureDatetime(measureDatetime);
 				weight.setKg(kg);
 				
 				list.add(weight);
@@ -45,10 +45,37 @@ public class JDBCWeightService implements WeightService{
 			
 			rs.close();
 			st.close();
-		
-		} catch (Exception e) {
+		}catch (Exception e) {
 			throw new ServiceException();
 		}
 		return list;
 	}
+	
+	public int insert(Weight weight) throws SQLException, ClassNotFoundException {
+		int result = 0;
+		
+		String sql = "INSERT INTO WEIGHT(PET_ID,MEASURE_DATETIME,KG) VALUES(?,?,?)"; //?에 데이터 꽂아넣을거임
+		
+		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
+		Class.forName("oracle.jdbc.OracleDriver"); 
+		Connection con = DriverManager.getConnection(url, "PETHARU","1357"); //드라이버 로드
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		System.out.println(weight.getMeasureDatetime());
+		st.setInt(1,weight.getPetId()); //데이터를 꽂음
+		st.setString(2,weight.getMeasureDatetime());
+		st.setFloat(3, weight.getKg());
+		
+		
+		result = st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+		return result;
+		
+	}
+	
+	
 }
+
