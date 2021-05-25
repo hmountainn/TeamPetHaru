d3b502a9f7171e6484bb67d89f7cf70c88d625ca
hyo2n <hyo_2n@naver.com>
2021-05-25 18:01:39 +0900

    diary comment, notice 수정

--- WebContent/community/notice/create.js
+++ WebContent/community/notice/create.js
@@ -2,16 +2,24 @@ window.addEventListener("load", function() {
     var main = document.querySelector("#main");
     var fileSelect = main.querySelector(".file-select");
     var inputFile = main.querySelector('input[type=file]');
-	var submitBtn = main.querySelector("button[type=submit]")
+	var submitBtn = main.querySelector("button[type=submit]");
 	
 	submitBtn.onclick = function() {
-		var request = new XMLHttpRequest();
-		request.onload = function() {
-			location.href = "./list.html";
+		var title = main.querySelector(".notice-title-create").value;
+		var content = main.querySelector("#notice-article-create").value;
+		
+		if (!title || !content) {
+			alert("내용을 입력해주세요.");
+			return false;
+		} else {
+			var request = new XMLHttpRequest();
+			request.onload = function() {
+				location.href = "./list.html";
+			}
+			request.open("POST", `./create`);
+			request.send(null);
 		}
-		request.open("POST", `./create`);
-		request.send(null);
-	}
+	};
 
 
     fileSelect.onclick = function(e) {
@@ -22,13 +30,13 @@ window.addEventListener("load", function() {
             'cancelable': true
         });
         inputFile.dispatchEvent(event);
-    }
+    };
 
     inputFile.oninput = function() {
         var span = document.createElement("span");
         fileSelect.insertAdjacentElement("afterend", span);
         span.classList.add("file-name");
         span.innerText = inputFile.files[0].name;
-    }
+    };
 
 })
